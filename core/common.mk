0ac6a7918e97f37129d39c64ff40176f678fca2e
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-02 22:34:22 +0100

    🔥 Improve Makefile common utils

--- core/common.mk
+++ core/common.mk
@@ -11,7 +11,7 @@ SHELL := $(shell which bash)
 
 .PHONY: help
 help: ## Show this help
-	@egrep -h '^[a-zA-Z_\/-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort -d | awk 'BEGIN {FS = ":.*?## "; printf "Usage: make \033[0;34mTARGET\033[0m \033[0;35m[ARGUMENTS]\033[0m\n\n"; printf "Targets:\n"}; {printf "  \033[33m%-25s\033[0m \033[0;32m%s\033[0m\n", $$1, $$2}'
+	@egrep -h '^[a-zA-Z0-9_\/-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort -d | awk 'BEGIN {FS = ":.*?## "; printf "Usage: make \033[0;34mTARGET\033[0m \033[0;35m[ARGUMENTS]\033[0m\n\n"; printf "Targets:\n"}; {printf "  \033[33m%-25s\033[0m \033[0;32m%s\033[0m\n", $$1, $$2}'
 
 .PHONY: enable
 enable: ## Enable service
