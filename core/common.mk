ccd2cfceb1a05261f91206ebf4069115552e4ee1
Borja Paz Rodríguez <borjapazr@gmail.com>
2021-12-02 18:04:14 +0100

    🔥 Improve Makefile common utils

--- core/common.mk
+++ core/common.mk
@@ -1,41 +1,17 @@
-## Colors ##
-BLACK := $(shell tput -Txterm setaf 0)
-RED := $(shell tput -Txterm setaf 1)
-GREEN := $(shell tput -Txterm setaf 2)
-YELLOW := $(shell tput -Txterm setaf 3)
-BLUE := $(shell tput -Txterm setaf 4)
-MAGENTA := $(shell tput -Txterm setaf 5)
-CYAN := $(shell tput -Txterm setaf 6)
-WHITE := $(shell tput -Txterm setaf 7)
-RESET := $(shell tput -Txterm sgr0)
-
-## Help function ##
-HELP_FUN = \
-    %help; \
-    while(<>) { push @{$$help{$$2 // 'Targets'}}, [$$1, $$3] if /^([a-zA-Z\-]+)\s*:.*\#\#(?:@([a-zA-Z\-]+))?\s(.*)$$/ }; \
-    print "Usage: make ${BLUE}TARGET${RESET} ${MAGENTA}[ARGUMENTS]${RESET}\n\n"; \
-    for (sort keys %help) { \
-    print "${WHITE}$$_:${RESET}\n"; \
-    for (sort {$$a->[0] cmp $$b->[0]} @{$$help{$$_}}) { \
-    $$sep = " " x (15 - length $$_->[0]); \
-    print "  ${YELLOW}$$_->[0]${RESET}$$sep${GREEN}$$_->[1]${RESET}\n"; \
-    }; \
-    print "\n"; }
-
 ## Docker commands ##
 DOCKER := docker
 DOCKER_COMPOSE := docker-compose
 DOCKER_COMPOSE_FILE := $(ROOT_DIR)/docker-compose.yml
 
-## Set 'sh' as default shell ##
-SHELL = /bin/sh
-## Set 'help' target as the default goal ##
+## Set 'bash' as default shell
+SHELL := $(shell which bash)
+
+## Set 'help' target as the default goal
 .DEFAULT_GOAL := help
 
-## Common targets ##
 .PHONY: help
 help: ## Show this help
-	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
+	@egrep -h '^[a-zA-Z_\/-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort -d | awk 'BEGIN {FS = ":.*?## "; printf "Usage: make \033[0;34mTARGET\033[0m \033[0;35m[ARGUMENTS]\033[0m\n\n"; printf "Targets:\n"}; {printf "  \033[33m%-25s\033[0m \033[0;32m%s\033[0m\n", $$1, $$2}'
 
 .PHONY: enable
 enable: ## Enable service
